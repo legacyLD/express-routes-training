cf705f9e5ef5e69f2a599fd51cafecbd08ad5da3
vedelopment <vedelopment@gmail.com>
2016-12-13 15:39:14 -0800

    resets progress in new repository

--- starter-code/public/app.js
+++ starter-code/public/app.js
@@ -1,4 +1,46 @@
 console.log('app.js connected');
 $(document).ready(function(){
   console.log('DOM ready');
+
+  $('#guess-number-form').on('submit', function(event) {
+      event.preventDefault();
+      console.log('request made');
+      $.ajax({
+        method: "GET",
+        url: "/pick-a-number",
+        data: $('#guess-number-form').serialize(),
+        dataType: "json",
+        success: OnSuccess,
+        error: OnError
+      });
+
+    function OnSuccess() {
+      console.log('ajax success');
+    }
+  });
+
+  $('#target-number-form').on('submit', function(event){
+    event.preventDefault();
+    $.ajax({
+      url: '/pick-a-number',
+      method: 'POST',
+      data: $('#target-number-form').serialize(),
+      success: handleTargetChangeSuccess,
+      error: handleError
+    });
+  });
+
+  function handleTargetChangeSuccess(targetResponse){
+    console.log(targetResponse);
+    $('#target-number-form')[0].reset();
+  }
+
+  function OnError() {
+    console.log('ajax error');
+  }
+
+});
+console.log('app.js connected');
+$(document).ready(function(){
+  console.log('DOM ready');
 });
